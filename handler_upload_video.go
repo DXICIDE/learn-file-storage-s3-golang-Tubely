3c04ba6cc156270511bb7acd7b91f5b38e58e454
DXICIDE <Rostaknc@seznam.cz>
2025-12-19 19:29:52 +0100

    started on video upload, working auth and parsing

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,70 @@
 package main
 
 import (
+	"fmt"
+	"mime"
 	"net/http"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	uploadLimit := http.MaxBytesReader(w, r.Body, 1<<30)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	//authenticatiing user
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	fmt.Println("uploading video", videoID, "by user", userID)
+
+	//video metadata to verify if user is the owner
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse get video from db", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Authenticated user is not the video owner", err)
+		return
+	}
+
+	//parsing the uploaded video
+
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+
+	file, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	headerType := header.Header.Values("Content-Type")
+
+	defer file.Close()
+
+	mediatype, _, err := mime.ParseMediaType(headerType[0])
+	if mediatype != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "the video is not an video", err)
+	}
+
+}
