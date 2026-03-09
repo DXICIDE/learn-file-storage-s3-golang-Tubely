9882985bd91da44642b7da687e1522292325ce57
DXICIDE <Rostaknc@seznam.cz>
2026-03-09 17:45:52 +0100

    FEAT: fully funtionning upload video

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
 	"mime"
 	"net/http"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
@@ -79,5 +83,48 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		respondWithError(w, http.StatusBadRequest, "the media is not an mp4 video", err)
 		return
 	}
+
+	fileTmp, err := os.CreateTemp("", "tubely-upload.mp4")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to create tmp file", err)
+		return
+	}
+
+	defer os.Remove(fileTmp.Name())
+	defer fileTmp.Close()
+	_, err = io.Copy(fileTmp, file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to copy the file to tmp", err)
+		return
+	}
+
+	_, err = fileTmp.Seek(0, io.SeekStart)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to reset the file pointer to start", err)
+		return
+	}
+	var putObject s3.PutObjectInput
+	rnd := make([]byte, 32)
+	filled, err := rand.Read(rnd)
+	if filled != 32 || err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldnt create file dst", err)
+	}
+	fileName := fmt.Sprintf("%x.mp4", rnd)
+
+	putObject.Bucket = &cfg.s3Bucket
+	putObject.Key = &fileName
+	putObject.Body = fileTmp
+	putObject.ContentType = &mediatype
+
+	_, err = cfg.s3Client.PutObject(r.Context(), &putObject)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to put object in the bucket", err)
+		return
+	}
+	videoUrl := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, fileName)
+	video.VideoURL = &videoUrl
+	cfg.db.UpdateVideo(video)
+
+	respondWithJSON(w, http.StatusOK, video)
 
 }
